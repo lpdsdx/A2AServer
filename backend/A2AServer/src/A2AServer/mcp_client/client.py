068ace06a5b11da20c6e309ad181f9d90a4d5339
Johnson <linghuchongxajh@qq.com>
2025-05-20 14:43:22 +0800

    "fastmcp==2.2.3目前得是这个版本，否则转化成tools时对doubao-think有问题"

--- backend/A2AServer/src/A2AServer/mcp_client/client.py
+++ backend/A2AServer/src/A2AServer/mcp_client/client.py
@@ -263,6 +263,7 @@ class MCPClient:
                 del self.responses[rid]
                 return resp
             timeout = os.getenv("MCP_TIMEOUT", 8)  #工具的最大超时时间
+            timeout = int(timeout)  #工具的最大超时时间
             start_time = asyncio.get_event_loop().time()
             response = await asyncio.wait_for(wait_for_response(), timeout=timeout)
             elapsed = asyncio.get_event_loop().time() - start_time
